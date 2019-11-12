1a5b6a81b525506b790f7be2a6688bad1ac2c27e
hoangbkcnnt98 <20166135@student.hust.edu.vn>
2019-11-13 00:25:53 +0700

    ok

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,7 +13,15 @@
     <link href="{{ asset('layouts/css/animate.css') }}" rel="stylesheet">
 	<link href="{{ asset('layouts/css/main.css') }}" rel="stylesheet">
 	<link href="{{ asset('layouts/css/responsive.css') }}" rel="stylesheet">
-    <link rel="shortcut icon" href="{{ asset('layouts/images') }}/ico/favicon.ico">
+	<link rel="shortcut icon" href="{{ asset('layouts/images') }}/ico/favicon.ico">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 	<!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 </head><!--/head-->
 
